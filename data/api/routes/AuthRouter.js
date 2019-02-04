b8e7b229b83122b93beec9fd7fd334af73325ae2
brooks <bpoltl1@gmail.com>
2019-02-04 14:02:06 -0600

    server running

--- data/api/routes/AuthRouter.js
+++ data/api/routes/AuthRouter.js
@@ -1,11 +1,1 @@
-const env = require ('dotenv').config ();
-const express = require ('express');
-const helmet = require ('helmet');
-const knex = require ('knex');
-const cors = require ('cors');
-const bcrypt = require ('bcryptjs');
-const knexConfig = require ('../../../knexfile');
-const jwt = require ('jsonwebtoken');
-const port = 5000;
-const server = express ();
 
